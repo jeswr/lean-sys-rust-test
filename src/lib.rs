cef0a19b9a04272cce75ec20fd9d026e268a166d
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-02-02 16:42:45 +0000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
-use lean_sys::{lean_array_push, lean_mk_empty_array, lean_mk_string, lean_obj_res};
+use lean_sys::{lean_mk_empty_array, lean_obj_res};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 
-pub fn arr() -> lean_obj_res {
-    let x = unsafe { lean_mk_empty_array() };
-    // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
-    // unsafe { lean_array_push(x, lean_mk_string("http://example.org/test".as_ptr())) };
-    // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
-    // unsafe { lean_array_push(x, lean_mk_string("http://example.org/predicate".as_ptr())) };
-    // unsafe { lean_array_push(x, lean_mk_string("Literal".as_ptr())) };
-    // unsafe { lean_array_push(x, lean_mk_string("belting".as_ptr())) };
-    return x;
-}
+// pub fn arr() -> lean_obj_res {
+//     let x = unsafe { lean_mk_empty_array() };
+//     // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
+//     // unsafe { lean_array_push(x, lean_mk_string("http://example.org/test".as_ptr())) };
+//     // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
+//     // unsafe { lean_array_push(x, lean_mk_string("http://example.org/predicate".as_ptr())) };
+//     // unsafe { lean_array_push(x, lean_mk_string("Literal".as_ptr())) };
+//     // unsafe { lean_array_push(x, lean_mk_string("belting".as_ptr())) };
+//     return x;
+// }
 
 #[cfg(test)]
 mod tests {
@@ -29,7 +29,8 @@ mod tests {
 
     #[test]
     fn arr_works() {
-        let result = arr();
-        assert_eq!(unsafe { lean_array_size(lean_mk_empty_array()) }, 6);
+        let a = unsafe { lean_mk_empty_array() };
+        // let _result = arr();
+        // assert_eq!(unsafe { lean_array_size(*lean_mk_empty_array()) }, 6);
     }
 }
