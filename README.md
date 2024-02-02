fb0988d823f3b7f7aea20f9a60498e4eaf5c7f56
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-02-02 16:49:23 +0000

    chore: add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# lean-sys test failure repro
+
+Running `cargo build; cargo test` results in
+
+```bash
+   Compiling oxrdf4lean v0.1.0 (/home/jesght/Documents/GitHub/oxrdf4lean)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.02s
+   Compiling oxrdf4lean v0.1.0 (/home/jesght/Documents/GitHub/oxrdf4lean)
+    Finished test [unoptimized + debuginfo] target(s) in 0.20s
+     Running unittests src/lib.rs (target/debug/deps/oxrdf4lean-3ecb2ebbd0f3f266)
+
+running 1 test
+error: test failed, to rerun pass `--lib`
+
+Caused by:
+  process didn't exit successfully: `/home/jesght/Documents/GitHub/oxrdf4lean/target/debug/deps/oxrdf4lean-3ecb2ebbd0f3f266` (signal: 11, SIGSEGV: invalid memory reference)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,9 @@
-use lean_sys::{lean_mk_empty_array, lean_obj_res};
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-
-// pub fn arr() -> lean_obj_res {
-//     let x = unsafe { lean_mk_empty_array() };
-//     // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
-//     // unsafe { lean_array_push(x, lean_mk_string("http://example.org/test".as_ptr())) };
-//     // unsafe { lean_array_push(x, lean_mk_string("NamedNode".as_ptr())) };
-//     // unsafe { lean_array_push(x, lean_mk_string("http://example.org/predicate".as_ptr())) };
-//     // unsafe { lean_array_push(x, lean_mk_string("Literal".as_ptr())) };
-//     // unsafe { lean_array_push(x, lean_mk_string("belting".as_ptr())) };
-//     return x;
-// }
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use lean_sys::lean_array_size;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    use lean_sys::lean_mk_empty_array;
 
     #[test]
     fn arr_works() {
-        let a = unsafe { lean_mk_empty_array() };
-        // let _result = arr();
-        // assert_eq!(unsafe { lean_array_size(*lean_mk_empty_array()) }, 6);
+        let _a = unsafe { lean_mk_empty_array() };
     }
 }
